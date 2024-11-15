881b43ebc63448c2d20b7155fe31c7baf67f4c71
Matt Henderson <mattcharlesh@gmail.com>
2024-11-15 12:47:36 -0500

    update home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,35 @@
-import { Text, View, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+import { View, StyleSheet } from 'react-native';
+
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label="Choose a photo" />
+                <Button label="Use this photo" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        alignItems: 'center',
+    },
+    imageContainer: {
+        flex: 1,
+    },
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
 });
